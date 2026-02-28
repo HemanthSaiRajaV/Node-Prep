ccbe1253b3d3861c65590c454e873bf6a1bf60fb
hemnath sai vanasetti <vanasettihemanth@example.com>
2026-02-28 21:53:31 +0530

    fix

--- notes/modules/coreModules/file-module/fs.js
+++ notes/modules/coreModules/file-module/fs.js
@@ -2,10 +2,10 @@ import { error } from "console";
 import fs from "fs";
 
 // creates the sync file, whenever user changes the data will be updated sync way  
-fs.writeFileSync('./notes/modules/coreModules/output/syncFile.txt', 'hi hemanth');
+fs.writeFileSync('./notes/modules/coreModules/file-module/output/syncFile.txt', 'hi hemanth');
 
 // creates the async file, whenever user changes the data will be updated sync way  
-fs.writeFile('./notes/modules/coreModules/output/asyncFile.txt', 'async data have created',(err)=>{
+fs.writeFile('./notes/modules/coreModules/file-module/output/asyncFile.txt', 'async data have created',(err)=>{
     if(err){
         console.log('print', error)
     }else{
@@ -14,11 +14,11 @@ fs.writeFile('./notes/modules/coreModules/output/asyncFile.txt', 'async data hav
 })
 
 // read the data from the file. whenever user changes the data will be updated sync way  
-const readMe = fs.readFileSync('./notes/modules/coreModules/output/readFile.txt', 'utf-8');
+const readMe = fs.readFileSync('./notes/modules/coreModules/file-module/output/readFile.txt', 'utf-8');
 console.log('fileData', readMe)
 
-// read the data from the file. whenever user changes the data will be updated async way but syntex is different 
-fs.readFile('./notes/modules/coreModules/output/readFile.txt', 'utf-8', (err, res)=>{
+// read the data from the file. whenever user changes the data will be updated async way but syntex is different
+fs.readFile('./notes/modules/coreModules/file-module/output/readFile.txt', 'utf-8', (err, res)=>{
     if(err){
         console.log('printError', err);
     }else{
