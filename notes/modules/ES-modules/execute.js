d943576adf5f0b3b1bbb608566b7ebfed1c3ca2c
hemnath sai vanasetti <vanasettihemanth@example.com>
2026-03-01 10:08:44 +0530

    fix

--- notes/modules/ES-modules/execute.js
+++ notes/modules/ES-modules/execute.js
@@ -1,4 +1,4 @@
 import {add, sub} from './math.js'
 
-console.log('adding values',add(8,22));
-console.log('sub the values', sub(88,55));
+// console.log('adding values',add(8,22));
+// console.log('sub the values', sub(88,55));
